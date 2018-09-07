b22a54513dfacd422f899636b26e026f84febec3
Rodrigo <rodrigoclimao@gmail.com>
2018-09-07 14:31:54 -0400

    a

---
+++ src/Model/Phone.java
@@ -0,0 +1,14 @@
+package Model;
+
+import javafx.scene.image.Image;
+
+public class Phone {
+
+private String make, model, OS, processorSize;
+private double memory, frontCameraRes, screensize, price, rearCameraRes;
+private Image phoneImage;
+
+}
+
+
+
